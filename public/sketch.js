0889033f39bdf5563f9498441e859ab361b0900e
karanbhagat <cv.ryn7@gmail.com>
2017-07-15 13:27:32 -0400

    Implemented basic interface for taking username as user input

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,25 @@
 //declare socket for the client and connect to server
 const socket = io.connect('localhost:8080');
+var startPage = true;
+var button, input;
+let data = {
+    x: 0,
+    y: 0,
+    name: "karan"
+};
 
 function setup() {
     createCanvas(600, 600);
     background(0);
-    socket.on('mouse', (data) => {
-        noStroke();
-        fill('blue');
-        ellipse(data.x, data.y, 15, 15);
-    });
+    takeUserInput();
+
+    if (!startPage) {
+        socket.on('mouse', (data) => {
+            noStroke();
+            fill('blue');
+            ellipse(data.x, data.y, 15, 15);
+        });
+    }
 }
 
 function draw() {
@@ -16,12 +27,47 @@ function draw() {
 
 //draw only on mouse drag
 function mouseDragged() {
-    var data = {
-        x: mouseX,
-        y: mouseY
+    if (!startPage) {
+        socket.emit('mouse', data);
+        noStroke(); // removes the boundary from the shape
+        fill('red');
+        ellipse(mouseX, mouseY, 15, 15);
     }
-    socket.emit('mouse', data);
-    noStroke(); // removes the boundary from the shape
-    fill('red');
-    ellipse(mouseX, mouseY, 15, 15);
+}
+
+/**
+ * Declare dom elements to show on the
+ * canvas for fetching username.
+ */
+function takeUserInput() {
+    //Text input
+    input = createInput("", String);
+    input.position(width/2 - 120, height/2);
+    input.attribute('placeholder', 'Enter your name!');
+    input.mousePressed(changeButtonTextColor);
+
+    //Button
+    button = createButton('Start Drawing');
+    button.position(width/2 + 30, height/2);
+    button.mouseClicked(takeUserName);
+}
+
+/**
+ * Verify if user provided an input
+ */
+function takeUserName() {
+    if (input.value() == "") {
+        button.style('color', 'red');
+        input.attribute('placeholder', 'Please Enter Name');
+    } else {
+        data.name = input.value();
+    }
+}
+
+/**
+ * If the color of button is red change back to
+ * black on user mouse press on input
+ */
+function changeButtonTextColor() {
+    button.style('color', 'black');
 }
